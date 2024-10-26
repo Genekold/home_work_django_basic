a96be7a5a71bf7b89b6e40c4f60fcf59c86d0a56
mihaylovea <mihailovevgenya@gmail.com>
2024-10-26 19:58:33 +0300

    исправление ошибки

--- catalog/views.py
+++ catalog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, "home.html")
+    return render(request, "catalog/home.html")
 
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    return render(request, 'catalog/contacts.html')
