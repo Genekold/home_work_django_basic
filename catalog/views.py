f3b17c7e1b7ca75e9a0a594d005077b665f8cd8e
mihaylovea <mihailovevgenya@gmail.com>
2024-11-17 17:55:25 +0300

    home_work_24

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from catalog.models import Product, Category
 
 def home(request):
     return render(request, "catalog/home.html")
@@ -7,3 +7,20 @@ def home(request):
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
+
+
+def index(request):
+    return render(request, 'catalog/base.html')
+
+
+def products(request):
+    product_list = Product.objects.all()
+    context = {'products': product_list}
+    return render(request, 'catalog/products.html', context)
+
+
+def product_info(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'catalog/product_info.html', context)
+
