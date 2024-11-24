db6c2b22dcc59e07ad6be484f88d4467ea0a18f0
mihaylovea <mihailovevgenya@gmail.com>
2024-11-24 12:01:36 +0300

    home work 25 product finaly

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView,UpdateView, DeleteView
 
-from catalog.models import Product, Category
+from catalog.models import Product
 
 
 class ProductListView(ListView):
@@ -10,3 +11,20 @@ class ProductListView(ListView):
 
 class ProductDetailView(DetailView):
     model = Product
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ('name', 'description', 'photo', 'category', 'price')
+    success_url = reverse_lazy("catalog:product_list")
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ('name', 'description', 'photo', 'category', 'price')
+    success_url = reverse_lazy("catalog:product_list")
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy("catalog:product_list")
